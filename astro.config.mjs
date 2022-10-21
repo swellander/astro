f7d683f3f14dc8cbef4abd06025f539afb4369ca
Sam Wellander <samwellander@gmail.com>
2022-10-20 20:59:39 -0700

    chore: configure vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,11 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   integrations: [react(), tailwind()],
+  output: "server",
 });
