f05f699a0be00cdbe6c74ae505c8cde5c1bd0c7f
Sam Wellander <samwellander@gmail.com>
2022-10-25 17:10:14 -0700

    TEMP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   integrations: [react()],
+  output: "server",
 });
